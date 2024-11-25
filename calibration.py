5b935f71d4eec4bb517f20ed13fd724a4ff30e4d
zameermd9 <zameerhussain27101999@gmail.com>
2024-11-25 09:48:12 +0100

    calibration preparing object points

--- calibration.py
+++ calibration.py
@@ -7,3 +7,6 @@ chessboardSize = (8, 6)
 frameSize = (640, 480)
 
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30)
+
+objp = np.zeros((chessboardSize[0] * chessboardSize[1], 3), np.float32)
+objp[:,:2] = np.mgrid[0:chessboardSize[0], 0:chessboardSize[1]].T.reshape(-1, 2)
