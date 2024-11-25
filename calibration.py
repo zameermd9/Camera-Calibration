154050613862132289e3779c394cdaddf7edfb58
zameermd9 <zameerhussain27101999@gmail.com>
2024-11-25 09:53:57 +0100

    calibration converting gray scale

--- calibration.py
+++ calibration.py
@@ -11,4 +11,13 @@ criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30)
 objp = np.zeros((chessboardSize[0] * chessboardSize[1], 3), np.float32)
 objp[:,:2] = np.mgrid[0:chessboardSize[0], 0:chessboardSize[1]].T.reshape(-1, 2)
 
+objpoints = []  
+imgpoints = []
 images = glob.glob('/Users/zameerhussainmohammed/Desktop/camera calibration2/images/*.png')
+
+for image in images:
+    img = cv.imread(image)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    ret, corners = cv.findChessboardCorners(gray, chessboardSize, None)
+    
+
