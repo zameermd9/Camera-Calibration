fb7b1c50f2e621ba8c547dd09d5f23632ac4ee02
zameermd9 <zameerhussain27101999@gmail.com>
2024-11-25 09:34:05 +0100

    real sense fourth commit

--- realsense_camera.py
+++ realsense_camera.py
@@ -6,4 +6,7 @@ pipeline = rs.pipeline()
 pipeline.start(rs.config().enable_stream(640,480,rs.format.bgr8, 30))
 
 frames = pipeline.wait_for_frames()
+color_frame = frames.get_color_frame()
+cv.imwrite("calibration_image.png", np.asanyarray(color_frame.get_data()))
+pipeline.stop()
 
