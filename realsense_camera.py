b2cb2db66f8e589f27a5b7ec2a3e1bd40dfb1438
zameermd9 <zameerhussain27101999@gmail.com>
2024-11-25 09:30:06 +0100

    real sense third commit

--- realsense_camera.py
+++ realsense_camera.py
@@ -2,3 +2,8 @@ import pyrealsense2 as rs
 import cv
 import numpy as np
 
+pipeline = rs.pipeline()
+pipeline.start(rs.config().enable_stream(640,480,rs.format.bgr8, 30))
+
+frames = pipeline.wait_for_frames()
+
