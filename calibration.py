c45c385e28156fe5a29c5e889267a5b8af60af21
zameermd9 <zameerhussain27101999@gmail.com>
2024-11-25 10:18:04 +0100

    calibration final

--- calibration.py
+++ calibration.py
@@ -6,7 +6,7 @@ import pickle
 chessboardSize = (8, 6) 
 frameSize = (640, 480)
 
-criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30)
+criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30,0.001)
 
 objp = np.zeros((chessboardSize[0] * chessboardSize[1], 3), np.float32)
 objp[:,:2] = np.mgrid[0:chessboardSize[0], 0:chessboardSize[1]].T.reshape(-1, 2)
