04ccd39a3a07bf435f1b67031a6e06d6c095c2e2
zameermd9 <zameerhussain27101999@gmail.com>
2024-11-25 09:34:48 +0100

    real sense fifth commit

--- realsense_camera.py
+++ realsense_camera.py
@@ -3,10 +3,10 @@ import cv
 import numpy as np
 
 pipeline = rs.pipeline()
-pipeline.start(rs.config().enable_stream(640,480,rs.format.bgr8, 30))
+pipeline.start(rs.config().enable_stream(rs.stream.color,640,480,rs.format.bgr8, 30))
 
 frames = pipeline.wait_for_frames()
 color_frame = frames.get_color_frame()
-cv.imwrite("calibration_image.png", np.asanyarray(color_frame.get_data()))
+cv.imwrite("images.png", np.asanyarray(color_frame.get_data()))
 pipeline.stop()
 
